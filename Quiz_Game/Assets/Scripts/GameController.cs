d3e7c05b948fdd68b221409d4b7aa289dbfcf633
lancastw <wyatt9612@gmail.com>
2019-02-09 23:09:56 -0600

    Update to include assignment 2
    Assignment 2 and video

---
+++ Quiz_Game/Assets/Scripts/GameController.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+
+
+    public Text questionDisplayText;
+    public Text scoreDisplayText;
+    public Text timeRemainingDisplayText;
+    public Text percentageDisplayText;
+    public Text questionNumberDisplayText;
+    public SimpleObjectPool answerButtonObjectPool;
+    public Transform answerButtonParent;
+    public GameObject questionDisplay;
+    public GameObject roundEndDisplay;
+
+    private DataController dataController;
+    private RoundData currentRoundData;
+    private QuestionData[] questionPool;
+
+    private bool isRoundActive;
+    private float timeRemaining;
+    private int questionIndex;
+    private int playerScore;
+    private int questionNumber;
+    private float percentage;
+    private List<GameObject> answerButtonGameObjects = new List<GameObject>();
+
+    // Use this for initialization
+    void Start()
+    {
+        dataController = FindObjectOfType<DataController>();
+        currentRoundData = dataController.GetCurrentRoundData();
+        questionPool = currentRoundData.questions;
+        timeRemaining = currentRoundData.timeLimitInSeconds;
+        UpdateTimeRemainingDisplay();
+
+        playerScore = 0;
+        questionIndex = 0;
+        questionNumber = 1;
+        percentage = 100;
+
+        ShowQuestion();
+        isRoundActive = true;
+        Debug.Log("starting");
+    }
+
+    private void ShowQuestion()
+    {
+        RemoveAnswerButtons();
+        QuestionData questionData = questionPool[questionIndex];
+        questionDisplayText.text = questionData.questionText;
+
+        for (int i = 0; i < questionData.answers.Length; i++)
+        {
+            GameObject answerButtonGameObject = answerButtonObjectPool.GetObject();
+            Debug.Log("add button");
+            answerButtonGameObjects.Add(answerButtonGameObject);
+            answerButtonGameObject.transform.SetParent(answerButtonParent);
+
+            AnswerButton answerButton = answerButtonGameObject.GetComponent<AnswerButton>();
+            answerButton.Setup(questionData.answers[i]);
+
+        }
+        
+        Debug.Log(playerScore);
+        Debug.Log(questionNumber);
+        Debug.Log(percentage);
+        
+        questionNumberDisplayText.text = "Question Number: " + questionNumber++;
+       
+    }
+
+    private void RemoveAnswerButtons()
+    {
+        while (answerButtonGameObjects.Count > 0)
+        {
+            answerButtonObjectPool.ReturnObject(answerButtonGameObjects[0]);
+            answerButtonGameObjects.RemoveAt(0);
+           
+        }
+
+    }
+
+    public void AnswerButtonClicked(bool isCorrect)
+    {
+        if (isCorrect)
+        {
+            Debug.Log("correct answer");
+            playerScore += currentRoundData.pointsAddedForCorrectAnswer;
+            scoreDisplayText.text = "Correct Answers: " + playerScore.ToString();
+           
+        }
+        percentage = (((float)(playerScore) / questionNumber) * 100);
+        percentageDisplayText.text = "Percent Correct: " + percentage + "%";
+        if (questionPool.Length > questionIndex + 1)
+        {
+            questionIndex++;
+            ShowQuestion();
+        }
+        else
+        {
+
+            EndRound();
+        }
+
+    }
+
+    public void EndRound()
+    {
+        isRoundActive = false;
+
+        questionDisplay.SetActive(false);
+        roundEndDisplay.SetActive(true);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MenuScreen");
+    }
+
+    private void UpdateTimeRemainingDisplay()
+    {
+        timeRemainingDisplayText.text = "Time: " + Mathf.Round(timeRemaining).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isRoundActive)
+        {
+            timeRemaining -= Time.deltaTime;
+            UpdateTimeRemainingDisplay();
+
+            if (timeRemaining <= 0f)
+            {
+                EndRound();
+            }
+
+        }
+    }
+}
